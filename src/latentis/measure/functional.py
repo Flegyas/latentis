5837b3f0656b4c71463866f7dcee733bd375aa9d
crisostomi <donatocrisostomi@gmail.com>
2023-12-02 20:58:52 +0100

    add functional

--- src/latentis/measure/functional.py
+++ src/latentis/measure/functional.py
@@ -1,77 +1,87 @@
-import functools
+import latentis
+from latentis.measure._metrics import Metric
+import torch
+import math
+from enum import auto
+
+try:
+    # be ready for 3.10 when it drops
+    from enum import StrEnum
+except ImportError:
+    from backports.strenum import StrEnum
+
+from enum import auto
+import torch
 import math
 
-import torch
 
-from latentis.space import LatentSpace
+def linear_cka(space1: torch.Tensor, space2: torch.Tensor):
+    return cka(space1, space2, hsic=linear_hsic)
 
+def rbf_cka(space1: torch.Tensor, space2: torch.Tensor, sigma:float=None):
+    return cka(space1, space2, hsic=kernel_hsic, sigma=sigma)
 
-def linear_cka(x: torch.Tensor, y: torch.Tensor):
-    return cka(x, y, hsic=linear_hsic)
+def cka(space1: torch.Tensor, space2: torch.Tensor, hsic: callable, sigma:float=None, device=None, tolerance=1e-6):
 
+    if isinstance(space1, latentis.LatentSpace):
+        space1 = space1.vectors
 
-def rbf_cka(x: torch.Tensor, y: torch.Tensor, *, sigma: float = None):
-    return cka(x, y, hsic=functools.partial(kernel_hsic, sigma=sigma))
+    if isinstance(space2, latentis.LatentSpace):
+        space2 = space2.vectors
 
+    assert space1.shape[0] == space2.shape[0], "X and Y must have the same number of samples."
 
-def cka(x: torch.Tensor, y: torch.Tensor, *, hsic: callable, tolerance=1e-6):
-    if isinstance(x, LatentSpace):
-        x = x.vectors
+    space1 = space1.to(device)
+    space2 = space2.to(device)
 
-    if isinstance(y, LatentSpace):
-        y = y.vectors
+    numerator = hsic(space1, space2, sigma)
 
-    assert x.shape[0] == y.shape[0], "X and Y must have the same number of samples."
-
-    numerator = hsic(x, y)
-
-    var1 = torch.sqrt(hsic(x, x))
-    var2 = torch.sqrt(hsic(y, y))
+    var1 = torch.sqrt(hsic(space1, space1, sigma))
+    var2 = torch.sqrt(hsic(space2, space2, sigma))
 
     cka_result = numerator / (var1 * var2)
 
-    assert 0 - tolerance <= cka_result <= 1 + tolerance, "CKA value must be between 0 and 1."
+    assert 0 - tolerance <= cka_result <= 1 + tolerance , "CKA value must be between 0 and 1."
 
     return cka_result
 
-
-def linear_hsic(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    """Compute HSIC for linear kernels.
+def linear_hsic(X: torch.Tensor, Y: torch.Tensor, *args, **kwargs) -> torch.Tensor:
+    """
+    Compute HSIC for linear kernels.
 
     This method is used in the computation of linear CKA.
 
     Args:
-        X: shape (N, D), first embedding matrix.
-        Y: shape (N, D'), second embedding matrix.
+        X, Y: Input matrices (num_samples, embedding_dim).
 
     Returns:
         The computed HSIC value.
     """
+
     # inter-sample similarity matrices for both spaces ~(N, N)
-    L_X = x @ x.T
-    L_Y = y @ y.T
+    L_X = X @ X.T
+    L_Y = Y @ Y.T
 
-    return torch.sum(center_kernel_matrix(L_X) * center_kernel_matrix(L_Y))
+    return torch.sum(centering(L_X) * centering(L_Y))
 
-
-def kernel_hsic(x: torch.Tensor, y: torch.Tensor, *, sigma):
-    """Compute HSIC (Hilbert-Schmidt Independence Criterion) for RBF kernels.
+def kernel_hsic(X: torch.Tensor, Y:torch.Tensor, sigma):
+    """
+    Compute HSIC (Hilbert-Schmidt Independence Criterion) for RBF kernels.
 
     This is used in the computation of kernel CKA.
 
     Args:
-        X: shape (N, D), first embedding matrix.
-        Y: shape (N, D'), second embedding matrix.
+        X, Y: Input matrices (num_samples, embedding_dim).
         sigma: The RBF kernel width.
 
     Returns:
         The computed HSIC value.
     """
-    return torch.sum(center_kernel_matrix(rbf(x, sigma=sigma)) * center_kernel_matrix(rbf(y, sigma=sigma)))
+    return torch.sum(centering(rbf(X, sigma)) * centering(rbf(Y, sigma)))
 
-
-def center_kernel_matrix(k: torch.Tensor) -> torch.Tensor:
-    """Center the kernel matrix K using the centering matrix H = I_n - (1/n) 1 * 1^T. (Eq. 3 in the paper).
+def centering(K: torch.Tensor) -> torch.Tensor:
+    """
+    Center the kernel matrix K using the centering matrix H = I_n - (1/n) 1 * 1^T. (Eq. 3 in the paper)
 
     This method is used in the calculation of HSIC.
 
@@ -81,16 +91,16 @@ def center_kernel_matrix(k: torch.Tensor) -> torch.Tensor:
     Returns:
         The centered kernel matrix.
     """
-    n = k.shape[0]
-    unit = torch.ones([n, n]).type_as(k)
-    identity_mat = torch.eye(n).type_as(k)
+    n = K.shape[0]
+    unit = torch.ones([n, n]).type_as(K)
+    identity_mat = torch.eye(n).type_as(K)
     H = identity_mat - unit / n
 
-    return H @ k @ H
+    return H @ K @ H
 
-
-def rbf(x: torch.Tensor, *, sigma=None):
-    """Compute the RBF (Radial Basis Function) kernel for a matrix X.
+def rbf(X: torch.Tensor, sigma=None):
+    """
+    Compute the RBF (Radial Basis Function) kernel for a matrix X.
 
     If sigma is not provided, it is computed based on the median distance.
 
@@ -101,8 +111,9 @@ def rbf(x: torch.Tensor, *, sigma=None):
     Returns:
         The RBF kernel matrix.
     """
-    GX = x @ x.T
-    KX = torch.diag(GX).type_as(x) - GX + (torch.diag(GX) - GX).T
+
+    GX = X @ X.T
+    KX = torch.diag(GX) - GX + (torch.diag(GX) - GX).T
 
     if sigma is None:
         mdist = torch.median(KX[KX != 0])
