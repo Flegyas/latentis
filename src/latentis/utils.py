c36b894308980193f2e6237db3cafa2ca36b935d
Valentino Maiorca <valentino@maiorca.xyz>
2024-01-17 18:26:16 +0100

    Refactor VectorSource
    Add keys field and SerializableMixin to it

--- src/latentis/utils.py
+++ src/latentis/utils.py
@@ -2,11 +2,15 @@ import logging
 import os
 import random
 from contextlib import contextmanager
-from typing import Optional
+from pathlib import Path
+from typing import Dict, Optional, Sequence
 
 import dotenv
 import numpy as np
+import pandas as pd
 import torch
+
+from latentis.types import SerializableMixin
 
 pylogger = logging.getLogger(__name__)
 
@@ -127,3 +131,50 @@ def seed_everything(seed: Optional[int] = None) -> int:
     torch.cuda.manual_seed_all(seed)
 
     return seed
+
+
+class BiMap(SerializableMixin):
+    def __init__(self, x: Sequence[str], y: Sequence[int]):
+        assert len(x) == len(y), "x and y must have the same length"
+        self._x2y: Dict[str, int] = {k: v for k, v in zip(x, y)}
+        self._y2x: Dict[int, str] = {v: k for k, v in self._x2y.items()}
+
+    def contains_x(self, x: str) -> bool:
+        return x in self._x2y
+
+    def contains_y(self, y: int) -> bool:
+        return y in self._y2x
+
+    def get_x(self, y: int) -> str:
+        return self._y2x[y]
+
+    def get_y(self, x: str) -> int:
+        return self._x2y[x]
+
+    def add(self, x: str, y: int):
+        assert x not in self._x2y, f"X `{x}` already exists"
+        assert y not in self._y2x, f"Y `{y}` already exists"
+
+        self._x2y[x] = y
+        self._y2x[y] = x
+
+    def add_all(self, x: Sequence[str], y: Sequence[int]):
+        assert len(x) == len(y), "x and y must have the same length"
+        for x_i, y_i in zip(x, y):
+            self.add(x_i, y_i)
+
+    def __len__(self) -> int:
+        assert len(self._x2y) == len(self._y2x)
+        return len(self._x2y)
+
+    def save_to_disk(self, target_path: Path):
+        df = pd.DataFrame({"x": list(self._x2y.keys()), "y": list(self._x2y.values())})
+        df.to_csv(target_path, sep="\t", index=False)
+
+    @classmethod
+    def load_from_disk(cls, path: Path) -> "BiMap":
+        mapping = pd.read_csv(path, sep="\t")
+        return cls(x=mapping["x"].tolist(), y=mapping["y"].tolist())
+
+    def __repr__(self) -> str:
+        return repr(self._x2y)
--- src/latentis/vector_source.py
+++ src/latentis/vector_source.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 import logging
 from abc import abstractmethod
 from pathlib import Path
+from typing import Optional, Sequence
 
 import torch
 
 from latentis.types import SerializableMixin
+from latentis.utils import BiMap
 
 pylogger = logging.getLogger(__name__)
 
@@ -46,10 +48,21 @@ class VectorSource(metaclass=VectorSourceMeta):
     def __eq__(self, __value: VectorSource) -> bool:
         raise NotImplementedError
 
+    @abstractmethod
+    def add_vectors(self, vectors: torch.Tensor, keys: Optional[Sequence[str]] = None) -> VectorSource:
+        raise NotImplementedError
 
-class InMemorySource(VectorSource, SerializableMixin):
-    def __init__(self, vectors: torch.Tensor):
+    @abstractmethod
+    def get_vector_by_key(self, key: str) -> torch.Tensor:
+        raise NotImplementedError
+
+
+class TensorSource(VectorSource, SerializableMixin):
+    def __init__(self, vectors: torch.Tensor, keys: Optional[Sequence[str]] = None):
+        assert keys is None or len(keys) == vectors.size(0), "Keys must be None or have the same length as vectors"
         self._vectors = vectors
+        keys = keys or []
+        self._keys2offset = BiMap(x=keys, y=range(len(keys)))
 
     def shape(self) -> torch.Size:
         return self._vectors.shape
@@ -60,8 +73,8 @@ class InMemorySource(VectorSource, SerializableMixin):
     def __len__(self) -> int:
         return self._vectors.size(0)
 
-    def __eq__(self, __value: InMemorySource) -> bool:
-        assert isinstance(__value, InMemorySource), f"Expected {InMemorySource}, got {type(__value)}"
+    def __eq__(self, __value: TensorSource) -> bool:
+        assert isinstance(__value, TensorSource), f"Expected {TensorSource}, got {type(__value)}"
         return torch.allclose(self._vectors, __value._vectors)
 
     def as_tensor(self) -> torch.Tensor:
@@ -69,7 +82,29 @@ class InMemorySource(VectorSource, SerializableMixin):
 
     def save_to_disk(self, parent_dir: Path):
         torch.save(self._vectors, parent_dir / "vectors.pt")
+        self._keys2offset.save_to_disk(parent_dir / "mapping.tsv")
 
     @classmethod
-    def load_from_disk(cls, path: Path) -> InMemorySource:
-        return cls(torch.load(path / "vectors.pt"))
+    def load_from_disk(cls, path: Path) -> TensorSource:
+        vectors = torch.load(path / "vectors.pt")
+        keys = BiMap.load_from_disk(path / "mapping.tsv")
+        result = cls(vectors=vectors)
+        result._keys2offset = keys  # TODO: ugly
+        return result
+
+    def add_vectors(self, vectors: torch.Tensor, keys: Optional[Sequence[str]] = None) -> TensorSource:
+        assert (keys is None) == (
+            len(self._keys2offset) == 0
+        ), "Keys must be provided only if the source already has keys"
+        if keys is not None:
+            assert len(keys) == vectors.size(0), "Keys must have the same length as vectors"
+            self._keys2offset.add_all(x=keys, y=range(len(self._keys2offset), len(self._keys2offset) + len(keys)))
+
+        return TensorSource(torch.cat([self._vectors, vectors], dim=0))
+
+    def get_vector_by_key(self, key: str) -> torch.Tensor:
+        assert len(self._keys2offset) > 0, "This source does not have keys enabled"
+        try:
+            return self[self._keys2offset.get_y(key)]
+        except KeyError:
+            raise KeyError(f"Key {key} not found in {self._keys2offset}")
