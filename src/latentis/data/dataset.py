769454a32c905d859711332557e20fe693a7a29f
Valentino Maiorca <valentino@maiorca.xyz>
2024-04-04 13:53:55 +0200

    [WIP] make DataProcessor objects via Pipeline

--- src/latentis/data/dataset.py
+++ src/latentis/data/dataset.py
@@ -5,14 +5,12 @@ from dataclasses import dataclass, field
 from datetime import datetime
 from enum import auto
 from pathlib import Path
-from typing import Any, Mapping, Optional, Sequence, Union
+from typing import Any, Mapping, Optional, Sequence
 
-import torch
 from datasets import DatasetDict
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 
 from latentis.data import DATA_DIR
-from latentis.nexus import space_index
 from latentis.serialize.io_utils import MetadataMixin, SerializableMixin, load_json, save_json
 from latentis.types import StrEnum
 
@@ -34,15 +32,14 @@ class DatasetProperty(StrEnum):
     pass
 
 
-# make it json serializable
 @dataclass(frozen=True)
 class Feature:
-    col_name: str
+    name: str
     data_type: DataType
     properties: Mapping[FeatureProperty, str] = field(default_factory=lambda: {})
 
     def __hash__(self):
-        return hash(self.col_name)
+        return hash((self.name, self.data_type, frozenset(self.properties.items())))
 
 
 @dataclass(frozen=True)
@@ -51,46 +48,49 @@ class FeatureMapping:
     target_col: str
 
 
-class DatasetView(torch.utils.data.Dataset):
-    def __init__(
-        self,
-        latentis_dataset: LatentisDataset,
-        encodings_key: Optional[Union[str, Sequence[str]]],
-        hf_x_keys: Optional[Union[str, Sequence[str]]],
-        hf_y_keys: Optional[Union[str, Sequence[str]]] = ("label",),
-    ):
-        super().__init__()
-        if isinstance(encodings_key, str):
-            encodings_key = [encodings_key]
-        if isinstance(hf_x_keys, str):
-            hf_x_keys = [hf_x_keys]
-        if isinstance(hf_y_keys, str):
-            hf_y_keys = [hf_y_keys]
+# class DatasetView(torch.utils.data.Dataset):
+#     def __init__(
+#         self,
+#         latentis_dataset: DatasetView,
+#         encodings_key: Optional[Union[str, Sequence[str]]],
+#         hf_x_keys: Optional[Union[str, Sequence[str]]],
+#         hf_y_keys: Optional[Union[str, Sequence[str]]] = ("label",),
+#     ):
+#         super().__init__()
+#         if isinstance(encodings_key, str):
+#             encodings_key = [encodings_key]
+#         if isinstance(hf_x_keys, str):
+#             hf_x_keys = [hf_x_keys]
+#         if isinstance(hf_y_keys, str):
+#             hf_y_keys = [hf_y_keys]
 
-        self.spaces = [space_index.load_item(item_key=key) for key in encodings_key]
-        spaces_split = set(space.split for space in self.spaces)
-        if len(spaces_split) > 1:
-            raise ValueError(f"Spaces {encodings_key} are not all from the same split!")
-        self.split = spaces_split.pop()
+#         self.spaces = [space_index.load_item(item_key=key) for key in encodings_key]
+#         spaces_split = set(space.split for space in self.spaces)
+#         if len(spaces_split) > 1:
+#             raise ValueError(f"Spaces {encodings_key} are not all from the same split!")
+#         self.split = spaces_split.pop()
 
-        self.data = latentis_dataset.hf_dataset[self.split]
-        self.encodings_key = encodings_key or []
-        self.hf_x_keys = hf_x_keys or []
-        self.hf_y_keys = hf_y_keys or []
+#         self.data = latentis_dataset.hf_dataset[self.split]
+#         self.encodings_key = encodings_key or []
+#         self.hf_x_keys = hf_x_keys or []
+#         self.hf_y_keys = hf_y_keys or []
 
-    def __getitem__(self, idx: int) -> Mapping[str, Any]:
-        sample = {
-            "encodings_key": [space[idx] for space in self.spaces],
-            "hf_x_keys": [self.data[idx][key] for key in self.hf_x_keys],
-            "hf_y_keys": [self.data[idx][key] for key in self.hf_y_keys],
-        }
-        return {key: value for key, value in sample.items() if value is not None and len(value) > 0}
-
-    def __len__(self) -> int:
-        return len(self.data)
+#     def __getitem__(self, idx: int) -> Mapping[str, Any]:
+#         sample = {
+#             "encodings_key": [space[idx] for space in self.spaces],
+#             "hf_x_keys": [self.data[idx][key] for key in self.hf_x_keys],
+#             "hf_y_keys": [self.data[idx][key] for key in self.hf_y_keys],
+#         }
+#         return {key: value for key, value in sample.items() if value is not None and len(value) > 0}
 
 
-class LatentisDataset(SerializableMixin, MetadataMixin):
+#     def __len__(self) -> int:
+#         return len(self.data)
+class DatasetView(SerializableMixin, MetadataMixin):
+    pass
+
+
+class HFDatasetView(DatasetView):
     def __init__(
         self,
         name: str,
@@ -109,7 +109,7 @@ class LatentisDataset(SerializableMixin, MetadataMixin):
             id_column in hf_dataset[split].column_names for split in hf_dataset.keys()
         ), f"ID column {id_column} not in all splits of dataset {hf_dataset}"
         assert all(
-            feature.col_name in hf_dataset[split].column_names for feature in features for split in hf_dataset.keys()
+            feature.name in hf_dataset[split].column_names for feature in features for split in hf_dataset.keys()
         ), f"Specified features not in all splits of dataset {hf_dataset}"
         assert 0 < perc <= 1, f"Percentage {perc} not in (0, 1]"
 
@@ -135,7 +135,7 @@ class LatentisDataset(SerializableMixin, MetadataMixin):
         cls,
         path: Path,
         load_hf_dataset: bool = True,
-    ) -> "LatentisDataset":
+    ) -> "DatasetView":
         assert (
             path / cls._METADATA_FILE_NAME
         ).exists(), f"Metadata file {path / cls._METADATA_FILE_NAME} does not exist! Are you sure about the parameters?"
@@ -149,14 +149,14 @@ class LatentisDataset(SerializableMixin, MetadataMixin):
         properties = metadata["properties"]
         features = [Feature(**feature) for feature in metadata["features"]]
 
-        dataset = LatentisDataset.__new__(cls)
+        dataset = DatasetView.__new__(cls)
 
-        dataset._name: str = metadata["name"]
-        dataset._hf_dataset: DatasetDict = hf_dataset
-        dataset._id_column: str = metadata["id_column"]
-        dataset._features: Sequence[Feature] = features
-        dataset._perc: float = metadata["perc"]
-        dataset._root_dir: Path = path
+        dataset._name = metadata["name"]
+        dataset._hf_dataset = hf_dataset
+        dataset._id_column = metadata["id_column"]
+        dataset._features[Feature] = features
+        dataset._perc = metadata["perc"]
+        dataset._root_dir = path
         dataset._properties = properties
 
         return dataset
@@ -187,7 +187,7 @@ class LatentisDataset(SerializableMixin, MetadataMixin):
 
     def get_feature(self, col_name: str) -> Optional[Feature]:
         for feature in self._features:
-            if feature.col_name == col_name:
+            if feature.target_name == col_name:
                 return feature
 
         return None
@@ -207,17 +207,18 @@ class LatentisDataset(SerializableMixin, MetadataMixin):
     def __repr__(self):
         return f"{self.__class__.__name__}(features={self._features}, perc={self._perc}, metadata={self.metadata})"
 
-    def get_dataset_view(
+    def select(
         self,
-        encodings_key: Sequence[str],
-        hf_x_keys: Sequence[str],
-        hf_y_keys: Sequence[str],
-    ) -> Dataset:
+        feature_keys: Sequence[str],
+    ):
         return DatasetView(
-            latentis_dataset=self,
-            encodings_key=encodings_key,
-            hf_x_keys=hf_x_keys,
-            hf_y_keys=hf_y_keys,
+            name=self._name,
+            hf_dataset=self._hf_dataset,
+            id_column=self._id_column,
+            features=[feature for feature in self._features if feature.target_name in feature_keys],
+            perc=self._perc,
+            properties=self._properties,
+            parent_dir=self._root_dir,
         )
 
     def get_dataloader(
@@ -231,7 +232,8 @@ class LatentisDataset(SerializableMixin, MetadataMixin):
         **kwargs,
     ):
         return DataLoader(
-            self.get_dataset_view(
+            DatasetView(
+                latentis_dataset=self,
                 encodings_key=space_id,
                 hf_x_keys=hf_x_keys,
                 hf_y_keys=hf_y_keys,
