e92329dd33944de868e9793f499a126235fa211a
Valentino Maiorca <valentino@maiorca.xyz>
2024-04-07 19:52:39 +0200

    Change LatentisDataset to DatasetView

--- src/latentis/data/encode.py
+++ src/latentis/data/encode.py
@@ -9,14 +9,14 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from latentis.data import DATA_DIR
-from latentis.data.dataset import Feature
-from latentis.data.processor import LatentisDataset
-from latentis.data.text_encoding import HFPooler, cls_pool
+from latentis.data.dataset import Feature, HFDatasetView
+from latentis.data.processor import DatasetView
+from latentis.data.text_encoding import HFPooler, Pooler, cls_pool
 from latentis.data.utils import default_collate
-from latentis.nexus import space_index
 from latentis.nn import LatentisModule
 from latentis.nn.encoders import TextHFEncoder
-from latentis.space import LatentSpace
+from latentis.space import Space
+from latentis.space.vector_source import HDF5Source
 
 pylogger = logging.getLogger(__name__)
 
@@ -45,49 +45,20 @@ DEFAULT_ENCODERS = {
 }
 
 
-def add_encoding(
-    item: LatentSpace,
-    save_source_model: bool,
-) -> LatentSpace:
-    # TODO: add consistency check to make sure that the item is compatible with the dataset
-    try:
-        existing_space = space_index.load_item(**item.properties)
-        existing_space.add_vectors(vectors=item.vectors, keys=item.keys)
+class IdentityPooling(Pooler):
+    def __init__(self, output_dim: int):
+        super().__init__(name="no_pooling", output_dim=output_dim)
 
-        # TODO: This is a hack, we are bypassing the index
-        target_path = space_index.get_item_path(**item.properties)
-        existing_space.save_to_disk(
-            target_path,
-            save_vector_source=True,
-            save_properties=False,
-            save_source_model=save_source_model,
-        )
-    except KeyError:
-        return space_index.add_item(
-            item=item,
-            save_args={
-                "save_vector_source": True,
-                "save_properties": True,
-                "save_source_model": save_source_model,
-            },
-        )
-
-
-def add_encodings(
-    items: Sequence[LatentSpace],
-    save_source_model: bool,
-) -> Sequence[str]:
-    for space in items:
-        add_encoding(item=space, save_source_model=save_source_model)
+    def forward(self, x):
+        return [(x, {})]
 
 
 def encode_feature(
-    dataset: LatentisDataset,
+    dataset: DatasetView,
     feature: str,
     model: LatentisModule,
     collate_fn: callable,
     encoding_batch_size: int,
-    store_every: int,
     num_workers: int,
     device: torch.device,
     save_source_model: bool = False,
@@ -107,6 +78,9 @@ def encode_feature(
         save_source_model (bool, optional): Save the source model. Defaults to False.
         poolers (Sequence[nn.Module], optional): Poolers to use for encoding. Defaults to None.
     """
+    if poolers is None or len(poolers) == 0:
+        poolers = [IdentityPooling(output_dim=model.output_dim)]
+
     feature: Feature = dataset.get_feature(feature)
 
     pylogger.info(f"Encoding {feature} for dataset {dataset._name}")
@@ -114,72 +88,63 @@ def encode_feature(
     model = model.to(device)
 
     for split, split_data in dataset.hf_dataset.items():
+        pooler2space = {
+            pooler: Space(
+                vector_source=HDF5Source(
+                    num_elements=len(split_data),
+                    dimension=pooler.output_dim,
+                    root_dir=DATA_DIR / dataset.name / "encodings" / split / feature.name / pooler.name,
+                ),
+                properties={
+                    **{f"model/{key}": value for key, value in model.properties.items()},
+                    "feature": feature.name,
+                    "split": split,
+                    "dataset": dataset.name,
+                    "pooler": pooler.__class__.__name__,
+                },
+            )
+            for pooler in poolers
+        }
+
         loader = DataLoader(
             split_data,
             batch_size=encoding_batch_size,
             pin_memory=device != torch.device("cpu"),
             shuffle=False,
             num_workers=num_workers,
-            collate_fn=functools.partial(collate_fn, model=model, feature=feature.col_name),
+            collate_fn=functools.partial(collate_fn, model=model, feature=feature.name),
         )
 
         for batch in tqdm(
-            loader, desc=f"Encoding `{split}` samples for feature {feature.col_name} using {model.item_id[:8]}"
+            loader, desc=f"Encoding `{split}` samples for feature {feature.name} using {model.item_id[:8]}"
         ):
             raw_encoding = model.encode(batch.to(device))
 
-            if not poolers:
-                assert isinstance(raw_encoding, torch.Tensor)
+            for pooler in poolers:
+                encoding2pooler_properties = pooler(**raw_encoding)
 
-                add_encoding(
-                    item=LatentSpace(
-                        vector_source=(raw_encoding.detach().cpu(), batch[dataset._id_column].cpu().tolist()),
-                        properties={
-                            **{f"model/{key}": value for key, value in model.properties.items()},
-                            "feature": feature.col_name,
-                            "split": split,
-                            "dataset": dataset.name,
-                        },
-                    ),
-                    save_source_model=save_source_model,
-                )
-            else:
-                for pooler in poolers:
-                    encoding2pooler_properties = pooler(**raw_encoding)
-
-                    for encoding, pooler_properties in encoding2pooler_properties:
-                        add_encoding(
-                            item=LatentSpace(
-                                vector_source=(encoding.detach().cpu(), batch[dataset._id_column].cpu().tolist()),
-                                properties={
-                                    **{f"model/{key}": value for key, value in model.properties.items()},
-                                    "feature": feature.col_name,
-                                    "split": split,
-                                    "dataset": dataset.name,
-                                    **pooler_properties,
-                                },
-                            ),
-                            save_source_model=save_source_model,
-                        )
+                for encoding, pooler_properties in encoding2pooler_properties:
+                    pooler2space[pooler].add_vectors(
+                        vectors=encoding, keys=batch[dataset._id_column].cpu().tolist(), write=True
+                    )
 
     model.cpu()
 
 
 if __name__ == "__main__":
-    for dataset, hf_encoder in itertools.product(["trec"], ["bert-base-cased"]):
-        dataset = LatentisDataset.load_from_disk(DATA_DIR / dataset)
+    for dataset_name, hf_encoder in itertools.product(["trec"], ["bert-base-cased"]):
+        dataset = HFDatasetView.load_from_disk(DATA_DIR / dataset_name)
 
-        encode_feature(
+        space = encode_feature(
             dataset=dataset,
             feature="text",
             model=TextHFEncoder(hf_encoder),
             collate_fn=default_collate,
             encoding_batch_size=256,
-            store_every=10,
             num_workers=0,
             save_source_model=False,
             poolers=[
-                HFPooler(layers=[12], pooling_fn=cls_pool),
+                HFPooler(layers=[12], pooling_fn=cls_pool, output_dim=768),
             ],
             device=torch.device("cpu"),
         )
