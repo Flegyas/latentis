8aedbe7edd93200d9e7b89e12fc6000ad50c662a
Valentino Maiorca <valentino@maiorca.xyz>
2024-04-07 19:54:00 +0200

    Add HDF5Source as a HDF5-based vector source

--- src/latentis/space/vector_source.py
+++ src/latentis/space/vector_source.py
@@ -5,12 +5,21 @@ from abc import abstractmethod
 from pathlib import Path
 from typing import Optional, Sequence, Union
 
+import h5py
+import numpy as np
 import torch
 
 from latentis.data.utils import BiMap
 from latentis.serialize.io_utils import SerializableMixin
 
 pylogger = logging.getLogger(__name__)
+
+
+def _torch_dtype_to_numpy(dtype: torch.dtype) -> np.dtype:
+    try:
+        return np.dtype(str(dtype).replace("torch.", "np."))
+    except TypeError:
+        return None
 
 
 _VECTOR_SOURCES = {}
@@ -50,16 +59,16 @@ class VectorSource(metaclass=VectorSourceMeta):
         raise NotImplementedError
 
     @abstractmethod
-    def add_vectors_(self, vectors: torch.Tensor, keys: Optional[Sequence[str]] = None) -> VectorSource:
-        raise NotImplementedError
-
-    @abstractmethod
     def get_vector_by_key(self, key: str) -> torch.Tensor:
         raise NotImplementedError
 
     @property
     @abstractmethod
     def keys(self) -> Sequence[str]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_vectors(self, vectors: torch.Tensor, keys: Optional[Sequence[str]] = None) -> VectorSource:
         raise NotImplementedError
 
 
@@ -101,17 +110,6 @@ class TensorSource(VectorSource, SerializableMixin):
         result._keys2offset = keys  # TODO: ugly
         return result
 
-    def add_vectors_(self, vectors: torch.Tensor, keys: Optional[Sequence[str]] = None) -> TensorSource:
-        assert (keys is None) == (
-            len(self._keys2offset) == 0
-        ), "Keys must be provided only if the source already has keys"
-        if keys is not None:
-            assert len(keys) == vectors.size(0), "Keys must have the same length as vectors"
-            self._keys2offset.add_all(x=keys, y=range(len(self._keys2offset), len(self._keys2offset) + len(keys)))
-
-        self._vectors = torch.cat([self._vectors, vectors], dim=0)
-        return self
-
     def get_vector_by_key(self, key: str) -> torch.Tensor:
         assert len(self._keys2offset) > 0, "This source does not have keys enabled"
         try:
@@ -122,3 +120,99 @@ class TensorSource(VectorSource, SerializableMixin):
     @property
     def keys(self) -> Sequence[str]:
         return self._keys2offset.x
+
+    def add_vectors(self, vectors: torch.Tensor, keys: Sequence[str] | None = None) -> VectorSource:
+        if vectors.ndim == 1:
+            vectors = vectors.unsqueeze(0)
+
+        assert (keys is None) == (
+            len(self._keys2offset) == 0
+        ), "Keys must be provided only if the source already has keys"
+        if keys is not None:
+            assert len(keys) == vectors.size(0), "Keys must have the same length as vectors"
+            self._keys2offset.add_all(x=keys, y=range(len(self._keys2offset), len(self._keys2offset) + len(keys)))
+
+        self._vectors = torch.cat([self._vectors, vectors], dim=0)
+        return self
+
+
+class HDF5Source(VectorSource):
+    def __init__(self, num_elements: int, dimension: int, root_dir: Path, dtype: torch.dtype = torch.float32) -> None:
+        super().__init__()
+        root_dir.mkdir(parents=True, exist_ok=True)
+        self.root_dir = root_dir
+        self.h5_file = h5py.File(root_dir / "data.h5", mode="w")
+        self.data: h5py.Dataset = self.h5_file.create_dataset(
+            "data",
+            shape=(num_elements, dimension),
+            dtype=_torch_dtype_to_numpy(dtype) or np.float32,
+            fillvalue=0,
+            maxshape=(num_elements, dimension),
+        )
+        self.h5_file.attrs["last_index"] = 0  # Tracking the last index used
+        self._keys2offset = BiMap(x=[], y=[])
+
+    @property
+    def shape(self) -> torch.Size:
+        return self.data.shape
+
+    @property
+    def dtype(self) -> torch.dtype:
+        return torch.dtype(self.data.dtype)
+
+    def __getitem__(self, index: Union[int, Sequence[int], slice]) -> torch.Tensor:
+        return torch.as_tensor(self.data[index])
+
+    def __len__(self) -> int:
+        return self.data.shape[0]
+
+    def as_tensor(self) -> torch.Tensor:
+        return torch.as_tensor(self.data)
+
+    def __eq__(self, __value: HDF5Source) -> bool:
+        assert isinstance(__value, HDF5Source), f"Expected {HDF5Source}, got {type(__value)}"
+        return torch.allclose(self.data, __value.data)
+
+    def add_vectors(
+        self, vectors: torch.Tensor, keys: Optional[Sequence[str]] = None, write: bool = False
+    ) -> VectorSource:
+        if vectors.ndim == 1:
+            vectors = vectors.unsqueeze(0)
+
+        # assert (keys is None) == (
+        #     len(self._keys2offset) == 0
+        # ), "Keys must be provided only if the source already has keys"
+        if keys is not None:
+            assert len(keys) == vectors.size(0), "Keys must have the same length as vectors"
+            self._keys2offset.add_all(x=keys, y=range(len(self._keys2offset), len(self._keys2offset) + len(keys)))
+
+        self.data[self.h5_file.attrs["last_index"] : self.h5_file.attrs["last_index"] + vectors.size(0)] = vectors
+        self.h5_file.attrs["last_index"] += vectors.size(0)
+
+        if write:
+            self.h5_file.flush()
+            self._save_mapping()
+
+        return self
+
+    def _save_mapping(self):
+        self._keys2offset.save_to_disk(self.root_dir / "mapping.tsv")
+
+    def to_tensor_source(self) -> TensorSource:
+        return TensorSource(vectors=self.as_tensor(), keys=self.keys)
+
+    # def save_to_disk(self, parent_dir: Path):
+    #     self.h5_file.close()
+    #     self._save_mapping()
+
+    @classmethod
+    def load_from_disk(cls, path: Path) -> HDF5Source:
+        data = path / "data.h5"
+        data = h5py.File(data, mode="r")
+
+        result = HDF5Source.__new__(HDF5Source)
+        result.h5_file = data
+        result.data = data["data"]
+        result._keys2offset = BiMap.load_from_disk(path / "mapping.tsv")
+
+        return result
